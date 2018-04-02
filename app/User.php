8a29b8c4c2b82fa997f1b2f0e3c673ecde91d61d
Tom Soisoonthorn <tom.soisoonthorn@gmail.com>
2018-04-02 14:23:18 -0700

    added note table migration. added notes model. udpated app description

--- app/User.php
+++ app/User.php
@@ -2,9 +2,22 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * Class User
+ * @package App
+ *
+ * @property int id
+ * @property string name
+ * @property string email
+ * @property string password
+ * @property string remember_token
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ */
 class User extends Authenticatable
 {
     use Notifiable;
---
+++ app/notes.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class notes
+ * @package App
+ *
+ * @property int id
+ * @property int author_id
+ * @property string title
+ * @property string body
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ */
+class notes extends Model
+{
+    //
+}
